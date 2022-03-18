d5bd3d479a5d654d11b3c501a69d4eca14aa34c0
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-03-17 21:16:05 -0500

    Se generan metodos en conexion cliente a DB

--- ProyectoWebCineJungla/src/java/datos/DBCliente.java
+++ ProyectoWebCineJungla/src/java/datos/DBCliente.java
@@ -3,6 +3,7 @@ package datos;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import logica.Cliente;
 
 /**
  *
@@ -15,30 +16,32 @@ public class DBCliente {
         cn = new DBConexion();
     }
     public ResultSet getClienteById(int id) throws SQLException{
-        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT id_cliente, "
-                + "nombre, "
-                + "apellido, "
-                + "correo, "
-                + "direccion, "
-                + "celular, "
-                + "cedula, "
-                + "id_ciudad,"
-                + "password "
+        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT idCliente, "
+                + "Nombre, "
+                + "PuntosJungla, "
+                + "Correo, "
+                + "MedioPago_idMedioPago "
                 + "FROM cliente "
-                + "WHERE id_cliente = "+id);
+                + "WHERE idCliente = "+id);
         ResultSet res = pstm.executeQuery();
         return res;
     }
     public ResultSet getClienteByCorreo(String correo) throws SQLException{
-        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT id_cliente, "
-                + "nombre, "
-                + "apellido, "
-                + "correo, "
-                + "direccion, "
-                + "celular, "
-                + "cedula, "
-                + "id_ciudad,"
-                + "password "
+        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT idCliente, "
+                + "Nombre, "
+                + "PuntosJungla, "
+                + "Correo, "
+                + "MedioPago_idMedioPago "
+                + "FROM cliente "
+                + "WHERE correo = ?");
+        pstm.setString(1, correo);
+        ResultSet res = pstm.executeQuery();
+        return res;
+    }
+    
+    public ResultSet getClienteLogin(String correo) throws SQLException{
+        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT idCliente, "
+                + "HashPws "
                 + "FROM cliente "
                 + "WHERE correo = ?");
         pstm.setString(1, correo);
@@ -47,37 +50,29 @@ public class DBCliente {
     }
     
     public ResultSet getClienteByNombre(String nombre) throws SQLException{
-        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT id_cliente, "
-                + "nombre, "
-                + "apellido, "
-                + "correo, "
-                + "direccion, "
-                + "celular, "
-                + "cedula, "
-                + "id_ciudad,"
-                + "password "
+        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT idCliente, "
+                + "Nombre, "
+                + "PuntosJungla, "
+                + "Correo, "
+                + "MedioPago_idMedioPago "
                 + "FROM cliente "
                 + "WHERE nombre = "+"\""+nombre+"\"");
         ResultSet res = pstm.executeQuery();
         return res;
     }
     public ResultSet getClientes() throws SQLException{
-        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT id_cliente, "
-                + "nombre, "
-                + "apellido, "
-                + "correo, "
-                + "direccion, "
-                + "celular, "
-                + "cedula, "
-                + "id_ciudad,"
-                + "password "
+        PreparedStatement pstm = cn.getConexion().prepareStatement("SELECT idCliente, "
+                + "Nombre, "
+                + "PuntosJungla, "
+                + "Correo, "
+                + "MedioPago_idMedioPago "
                 + "FROM cliente "
                 + "ORDER BY nombre ");
         ResultSet res = pstm.executeQuery();
         return res;
     }
-    /*public void insertarCliente(Cliente c){
-        try{
+    public void insertarCliente(Cliente c){
+        /*try{
             PreparedStatement pstm = cn.getConexion().prepareStatement("insert into cliente(nombre, "
                 + "apellido, "
                 + "correo, "
@@ -99,8 +94,8 @@ public class DBCliente {
             pstm.executeUpdate();
         }catch (SQLException e){
             System.out.println(e);
-        }
-    }*/
+        }*/
+    }
     public void eliminarCliente(int i) throws SQLException{
         PreparedStatement pstm = cn.getConexion().prepareStatement("delete from cliente where id_cliente = "+i);
             pstm.executeUpdate();
