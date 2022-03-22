d298c201c78394ed1a342f30437f5218bb5bcef7
unknown <manaranjoj@correo.udistrital.edu.co>
2022-03-22 17:08:55 -0500

    Cambios en automatizacion

--- ProyectoWebCineJungla/src/java/logica/Automatizacion/rellenoSalas.java
+++ ProyectoWebCineJungla/src/java/logica/Automatizacion/rellenoSalas.java
@@ -15,9 +15,7 @@ public class rellenoSalas {
     public rellenoSalas(int sala, int pelicula) throws SQLException {
         ResultSet DBf = funcionDB.getFuncionBySala(sala);
         while (DBf.next()) {
-            for (int x = 1; x <= DBf.getMetaData().getColumnCount(); x++) {
-                System.out.print(DBf.getString(x) + "\t");
-            }
+            
 
             System.out.println("");
         }
