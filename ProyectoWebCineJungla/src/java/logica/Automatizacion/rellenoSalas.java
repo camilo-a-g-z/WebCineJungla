fd1b1a16557ca53e6b7853650be177e3600db6e4
unknown <manaranjoj@correo.udistrital.edu.co>
2022-03-22 16:28:01 -0500

    Actualizacion relleno de salas

--- ProyectoWebCineJungla/src/java/logica/Automatizacion/rellenoSalas.java
+++ ProyectoWebCineJungla/src/java/logica/Automatizacion/rellenoSalas.java
@@ -1,13 +1,25 @@
 package logica.Automatizacion;
 
+import datos.DBFuncion;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Miguel
  */
 public class rellenoSalas {
 
-    public rellenoSalas(int sala, int pelicula) {
-        
+    DBFuncion funcionDB = new DBFuncion();
 
+    public rellenoSalas(int sala, int pelicula) throws SQLException {
+        ResultSet obtenidoBD = funcionDB.getFuncionBySala(sala);
+        while (obtenidoBD.next()) {
+            for (int x = 1; x <= obtenidoBD.getMetaData().getColumnCount(); x++) {
+                System.out.print(obtenidoBD.getString(x) + "\t");
+            }
+
+            System.out.println("");
+        }
     }
 }
