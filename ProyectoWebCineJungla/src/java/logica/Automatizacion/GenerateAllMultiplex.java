09ee3f504699b20777ed607f64823752aaa4cd30
camilo-a-g-z <cagarciaz@correo.udistrital.edu.co>
2022-04-02 22:42:34 -0500

    Creacion de clases para generar todo un multiplex

---
+++ ProyectoWebCineJungla/src/java/logica/Automatizacion/GenerateAllMultiplex.java
@@ -0,0 +1,17 @@
+package logica.Automatizacion;
+
+/**
+ *
+ * @author Camilo Garcia
+ */
+public class GenerateAllMultiplex {
+    private String NombreMultiplex = "";
+    private int CantidadSalas = 0;
+
+    public GenerateAllMultiplex(String NombreMultiplex, int CantidadSalas) {
+        this.NombreMultiplex = NombreMultiplex;
+        this.CantidadSalas = CantidadSalas;
+    }
+    
+    
+}
