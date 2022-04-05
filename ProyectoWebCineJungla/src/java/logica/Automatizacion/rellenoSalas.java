39d660363b543514bfef92ac41fd9f471bb2f688
unknown <manaranjoj@correo.udistrital.edu.co>
2022-04-05 00:22:49 -0500

    Actualizacion automatizacion

--- ProyectoWebCineJungla/src/java/logica/Automatizacion/rellenoSalas.java
+++ ProyectoWebCineJungla/src/java/logica/Automatizacion/rellenoSalas.java
@@ -85,6 +85,9 @@ public class rellenoSalas {
             //auxDurPel = objPel.getDuracion();
             pruebaPeliculas();
             auxDurPel = arregloTesteoPel.get(0).getDuracion();
+            auxMin = auxDurPel % 60;
+            auxHr = (int) auxDurPel / 60;
+            //System.out.println(auxDurPel + "=" + auxHr + ":" + auxMin);
 
             boolean coincidePelicula = false;
             int a = 0;
@@ -105,7 +108,9 @@ public class rellenoSalas {
                     if ((conf.get(i).getHorarioC().get(Calendar.YEAR) == (fechaActual.get(Calendar.YEAR)))
                             && (conf.get(i).getHorarioC().get(Calendar.MONTH) == (fechaActual.get(Calendar.MONTH)))
                             && (conf.get(i).getHorarioC().get(Calendar.DAY_OF_MONTH) == (fechaActual.get(Calendar.DAY_OF_MONTH)))) {
-                        System.out.println("ENTRA LINEA 95, por tanto se agregan funciones del dia en cuestion");
+                        //  System.out.println("ENTRA LINEA 95, por tanto se agregan funciones del dia en cuestion");
+                        //  System.out.println(conf.get(i).getHorarioC().get(Calendar.DAY_OF_MONTH) + " y " + (fechaActual.get(Calendar.DAY_OF_MONTH)));
+                        //  System.out.println(conf.get(i).getPelicula_idPelicula());
                         funcionDiaActual.add(conf.get(i));
                     }
                 }
@@ -115,7 +120,7 @@ public class rellenoSalas {
                     // System.out.println("Entra linea 102, el dia actual no tiene funciones");
 
                 } else {
-                    System.out.println("Entra LINEA 110, el dia actual SI tiene funciones");
+                    //System.out.println("Entra LINEA 110, el dia actual SI tiene funciones");
                     for (int j = 0; j < funcionDiaActual.size() && !coincidePelicula; j++) {
                         if (funcionDiaActual.get(j).getPelicula_idPelicula() == pelicula) {
                             System.out.println("AQUI ENTRA linea 106, por lo tanto la pelicula ya se esta proyectando este dia");
@@ -128,8 +133,7 @@ public class rellenoSalas {
 
                         //Si hay hueco antes de la primera funcion
                         if (funcionDiaActual.get(0).getHorarioC().get(Calendar.HOUR_OF_DAY) > (horaInicial.get(Calendar.HOUR_OF_DAY))) {
-                            auxMin = auxDurPel % 60;
-                            auxHr = (int) auxDurPel / 60;
+
                             Calendar fechaAux = Calendar.getInstance();
                             fechaAux = horaInicial;
                             fechaAux.add(Calendar.MINUTE, auxMin);
@@ -156,14 +160,15 @@ public class rellenoSalas {
 
                         //Insercion despues de la ultima funcion y antes de que sean las 12
                         if (!InsercionAntesFuncion) {
-                            System.out.println("AQUI ENTRA");
-                            auxMin = auxDurPel % 60;
-                            auxHr = (int) auxDurPel / 60;
+                            //System.out.println("AQUI ENTRA");
+
                             Calendar auxCal = Calendar.getInstance();
                             auxCal = funcionDiaActual.get(funcionDiaActual.size() - 1).getHorarioC();
-                            auxCal.add(Calendar.MINUTE, (int) funcionDiaActual.get(funcionDiaActual.size() - 1).getDuracion() / 60);
-                            auxCal.add(Calendar.HOUR_OF_DAY, funcionDiaActual.get(funcionDiaActual.size() - 1).getDuracion() % 60);
-                            auxCal.add(Calendar.MINUTE, auxMin);
+                            auxCal.add(auxCal.MINUTE, funcionDiaActual.get(funcionDiaActual.size() - 1).getDuracion() % 60);
+                            auxCal.add(auxCal.HOUR_OF_DAY, (int) (funcionDiaActual.get(funcionDiaActual.size() - 1).getDuracion() / 60));
+                            //System.out.println(auxCal.get(Calendar.HOUR_OF_DAY));
+                            //System.out.println(funcionDiaActual.get(funcionDiaActual.size() - 1).getDuracion());
+                            auxCal.add(auxCal.MINUTE, auxMin);
                             auxCal.add(Calendar.HOUR_OF_DAY, auxHr);
                             Date auxDate = new Date();
                             auxDate.setHours(auxCal.get(Calendar.HOUR_OF_DAY));
@@ -173,51 +178,58 @@ public class rellenoSalas {
                             auxDate2.setMinutes(horaFinal.get(Calendar.MINUTE));
                             if (auxDate.equals(auxDate2) || (auxDate.before(auxDate2))) {
                                 //Insertar Funcion
-
                                 System.out.println("ENTRA LINEA 180, hay tiempo suficiente para introducir la funcion despues de la ultima funcion y antes de las 12 de la noche");
                                 InsercionAntesFuncion = true;
                             }
 
                         }
-
-                        /*   if (!InsercionAntesFuncion && funcionDiaActual.size()>2) {
+                        //System.out.println(InsercionAntesFuncion);
+                        if (!InsercionAntesFuncion && funcionDiaActual.size() >= 2) {
                             //Verificar huecos entre funciones
-                            for (int i = 0; i < funcionDiaActual.size()-1; i++) {
+                            for (int i = 0; i < funcionDiaActual.size() - 1; i++) {
 
                                 System.out.println("Entro a la linea 190, no se ingresaron despues de las 12 y antes de la primera funcion"
                                         + " ni despues de la ultima funcion y antes de las 12");
+                                Calendar auxCal = Calendar.getInstance();
+                                Calendar auxCal2 = Calendar.getInstance();
 
-                                //Al horario de la funcion se le suma la duracion de la pelicula
-                                auxMin = funcionDiaActual.get(i).getDuracion() % 60;
+                                Date auxDate = new Date();
+                                Date auxDate2 = new Date();
 
-                                auxHr = (int) (funcionDiaActual.get(i).getDuracion() / 60);
+                                auxCal = funcionDiaActual.get(i).getHorarioC();
+                                auxCal.add(auxCal.MINUTE, (funcionDiaActual.get(i).getDuracion() % 60));
+                                auxCal.add(auxCal.HOUR_OF_DAY, (int) (funcionDiaActual.get(i).getDuracion() / 60));
+                                auxCal.add(auxCal.MINUTE, auxMin);
+                                auxCal.add(auxCal.HOUR_OF_DAY, auxHr);
 
-                                System.out.println(auxMin + " : " + auxHr);
-                                fechaDinamica = funcionDiaActual.get(i).getHorario();
-                                fechaDinamica.setMinutes(fechaDinamica.getMinutes() + auxMin);
-                                fechaDinamica.setHours(fechaDinamica.getHours() + auxHr);
-                                //Al horario y la duracion se le suma ademas la duracion de
-                                //la pelicula que queremos agregar
+                                auxDate.setHours(auxCal.get(Calendar.HOUR_OF_DAY));
+                                auxDate.setMinutes(auxCal.get(Calendar.MINUTE));
 
-                                auxMin = auxDurPel % 60;
-                                auxHr = (int) auxDurPel / 60;
-                                fechaDinamica.setMinutes(fechaDinamica.getMinutes() + auxMin);
-                                fechaDinamica.setHours(fechaDinamica.getHours() + auxHr);
-                                if (fechaDinamica.before(funcionDiaActual.get(i + 1).getHorario()) || (fechaDinamica == (funcionDiaActual.get(i + 1).getHorario()))) {
+                                auxCal2 = funcionDiaActual.get(i + 1).getHorarioC();
+                                auxDate2.setHours(auxCal2.get(auxCal2.HOUR_OF_DAY));
+                                auxDate2.setMinutes(auxCal2.get(auxCal2.MINUTE));
+
+                                System.out.println(auxDate.getHours() + ":" + auxDate.getMinutes());
+                                System.out.println(auxCal2.get(auxCal2.HOUR_OF_DAY) + ":" + auxCal2.get(auxCal2.MINUTE));
+                                System.out.println(auxDate.equals(auxDate2));
+                                System.out.println(auxDate.before(auxDate2));
+                                if (auxDate.equals(auxDate2) || (auxDate.before(auxDate2))) {
                                     //Ingresar funcion
+                                    System.out.println("ENTRA LINEA 215, se ingresa una funcion entre funciones");
                                 }
 
                             }
-                        }*/
+                        }
                     }
+
                 }
-                    funcionDiaActual.clear();
-                    coincidePelicula = false;
-                    InsercionAntesFuncion = false;
-                    //Se aumentara en un dia por cada bucle del while
-                    a = a + 1;
-                    fechaActual.add(Calendar.DATE, 1);
-                
+                funcionDiaActual.clear();
+                coincidePelicula = false;
+                InsercionAntesFuncion = false;
+                //Se aumentara en un dia por cada bucle del while
+                a = a + 1;
+                fechaActual.add(Calendar.DATE, 1);
+
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -252,9 +264,10 @@ public class rellenoSalas {
         prueba = new Funcion();
         prueba.setPelicula_idPelicula(4);
         prueba.setSala_idSala(1);
-        prueba.setHorarioC(generarCalendar(2022, 4, 15, 14, 40));
+        prueba.setHorarioC(generarCalendar(2022, 4, 15, 15, 40));
         prueba.setDuracion(85);
         conf.add(prueba);
+
     }
 
     public void pruebaPeliculas() {
